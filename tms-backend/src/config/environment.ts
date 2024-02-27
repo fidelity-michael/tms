39379f7123f02425b9be4d17fdf2c6fe556937ef
Michael <mixkok2001@gmail.com>
2024-02-27 16:41:34 +0200

    Added apiLimiter, authLimiter options in config

--- tms-backend/src/config/environment.ts
+++ tms-backend/src/config/environment.ts
@@ -19,6 +19,29 @@ export const config = {
       pass: process.env.DB_ROOT_PASSWORD,
     }
   },
+
+  // Session options
+  session: {
+    secret: process.env.SESSION_SECRET as any,
+    saveUninitialized: false,
+    resave: true,
+    rolling: true, //the session identifier cookie will expire in maxAge since the last response was sent
+    cookie: {
+      maxAge: parseInt(process.env.SESSION_MAX_AGE as any)
+    }
+  },
+
+  // Limiters for ddos attack prevention
+  apiLimiter: {
+    windowMs: 3*60*1000,    // 3 minutes
+    max: 300,               // limit each IP to 300 requests per windowMs (for each route)
+    message: "Too many requests!"
+  },
+  authLimiter: {
+    windowMs: 1*60*1000,    // 1 minute
+    max: 10,               // limit each IP to 10 requests per windowMs (for auth route)
+    message: "Too many requests!"
+  },
 };
 
 
