d9a2abf48caa0bf70586fa9c86ded77e8b63819f
Michael <mixkok2001@gmail.com>
2024-07-15 23:54:55 +0300

    Updated User model and added user controller. Routes remain to be implemented

--- tms-backend/src/api/v1/users/user-model.ts
+++ tms-backend/src/api/v1/users/user-model.ts
@@ -1,18 +1,18 @@
-import { Document, Schema, Model, model } from 'mongoose';
-import { DefaultSchemaOptions } from '../../../models/shared';
+import { Document, Schema, Model, model } from "mongoose";
+import { DefaultSchemaOptions } from "../../../models/shared";
 
 // Interface declaration
 export interface IUser extends Document {
-  first_name: string,
-  last_name: string,
-  email: string,
-  password: string,
-  role: [string],
-  group: string,
-  status: string,
-  department: string,
-  date: Date,
-};
+  first_name: string;
+  last_name: string;
+  email: string;
+  password: string;
+  role: string[];
+  group: string;
+  status: string;
+  department: string;
+  date: Date;
+}
 
 // ------------------------------------------
 // Event definition
@@ -28,23 +28,21 @@ const userSchema = new Schema(
     department: { type: String, default: "FirstName" },
     date: { type: Date, default: Date.now() },
   },
-  { ...DefaultSchemaOptions }
+  { ...DefaultSchemaOptions },
 );
 
 // ------------------------------------------
 // Schema model exports
-export const UserModel: Model<IUser> = model<IUser>(
-  'User', userSchema, 'User'
-);
+export const UserModel: Model<IUser> = model<IUser>("User", userSchema, "User");
 
 export type User_t = {
-  first_name: string,
-  last_name: string,
-  email: string,
-  password: string,
-  role: [string],
-  group: string,
-  status: string,
-  department: string,
-  date: Date,
-}
+  first_name?: string;
+  last_name?: string;
+  email: string;
+  password: string;
+  role?: string[];
+  group: string;
+  status?: string;
+  department: string;
+  date?: Date;
+};
