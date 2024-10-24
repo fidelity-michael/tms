6dcd32aa3681d078d0b94c270fcce1bcb35c6825
Michael <mixkok2001@gmail.com>
2024-10-24 18:37:17 +0300

    Fixed CRUD fucntions in area-controller and fixed issue with initialization when database for areas is empty

--- tms-backend/src/api/v1/area/area-controller.ts
+++ tms-backend/src/api/v1/area/area-controller.ts
@@ -1,13 +1,14 @@
-import { Request, Response, Router } from 'express';
-import { IArea, AreaModel, Area_t } from './area-model';
-import { ResourceController } from '../../shared';
-import { StatusCodes } from 'http-status-codes';
-import { Logger } from '../../shared/utils/logger';
+import { Request, Response, Router } from "express";
+import { IArea, AreaModel, Area_t } from "./area-model";
+import { ResourceController } from "../../shared";
+import { StatusCodes } from "http-status-codes";
+import { Logger } from "../../shared/utils/logger";
 
-export class AreaController extends ResourceController<IArea>{
+export class AreaController extends ResourceController<IArea> {
   private logger: Logger = Logger.getInstance();
   constructor() {
     super(AreaModel);
+    this.checkAndInitialize();
   }
   /**
    * Apply all routes for area
@@ -17,12 +18,13 @@ export class AreaController extends ResourceController<IArea>{
   public applyRoutes(): Router {
     const router = Router();
     router
-      .get('/', this.getArea)
-      .get('/initialize', this.initializeArea)
-      .get('/:id', this.getAreaById)
-      .post('/', this.postArea)
-      .put('/:id', this.updateArea)
-      .delete('/:id', this.deleteArea);
+      .get("/", this.getArea)
+      // .get("/initialize", this.initializeArea)
+      .get("/:id", this.getAreaById)
+      .post("/", this.postArea)
+      .patch("/:areaId", this.updateArea)
+      .delete("/all", this.deleteAll)
+      .delete("/:areaId", this.deleteArea);
 
     return router;
   }
@@ -33,14 +35,12 @@ export class AreaController extends ResourceController<IArea>{
    * @param res
    */
   getArea = async (req: Request, res: Response) => {
-    this.logger.debug('getArea request');
+    this.logger.debug("getArea request");
     // you can pre-process the request here before passing it to the super class method
     const allAreas = await this.getAll(req, res);
     // you can process the data retrieved here before returning it to the client
-    return res
-      .status(StatusCodes.OK)
-      .json(allAreas);
-  }
+    return res.status(StatusCodes.OK).json(allAreas);
+  };
 
   /**
    * Creates a new area
@@ -48,14 +48,29 @@ export class AreaController extends ResourceController<IArea>{
    * @param res
    */
   postArea = async (req: Request, res: Response) => {
-    this.logger.debug('postArea request');
-    // you can pre-process the request here before passing it to the super class method
-    const area = await this.create(req, res);
-    // you can process the data retrieved here before returning it to the client
-    return res
-      .status(StatusCodes.OK)
-      .json(area);
-  }
+    this.logger.debug("postArea request");
+    // Create a new Area
+    try {
+      let area = null;
+
+      if (req.body.description.length > 0) {
+        area = new AreaModel({
+          name: req.body.area_name,
+          description: req.body.description,
+        });
+      } else {
+        area = new AreaModel({
+          name: req.body.area_name,
+          description: "No Description",
+        });
+      }
+
+      const saved_area = await area.save();
+      res.status(StatusCodes.OK).send({ area: area._id });
+    } catch (err) {
+      res.status(400).send(err);
+    }
+  };
 
   /**
    * Delete area by id
@@ -63,14 +78,26 @@ export class AreaController extends ResourceController<IArea>{
    * @param res
    */
   deleteArea = async (req: Request, res: Response) => {
-    this.logger.debug('deleteArea request');
-    // you can pre-process the request here before passing it to the super class method
-    const area = await this.delete(req.params.id, req, res); // WARN: maybe areaId
-    // you can process the data retrieved here before returning it to the client
-    return res
-      .status(StatusCodes.OK)
-      .json(area);
-  }
+    this.logger.debug("deleteArea request");
+    const area = await this.delete(req.params.areaId, req, res);
+    return res.status(StatusCodes.OK).json(area);
+  };
+
+  /**
+   * Delete all areas
+   * @param req
+   * @param res
+   */
+  deleteAll = async (req: Request, res: Response) => {
+    this.logger.debug("deleteAll request");
+    const allAreas = await this.getAll(req, res);
+    if (allAreas) {
+      for (const item of allAreas) {
+        await this.delete(item._id.toString(), req, res);
+      }
+      this.logger.success("All area items deleted");
+    }
+  };
 
   /**
    * Update area by id
@@ -78,12 +105,19 @@ export class AreaController extends ResourceController<IArea>{
    * @param res
    */
   updateArea = async (req: Request, res: Response) => {
-    this.logger.debug('updateArea request');
-    const area = await this.update(req.params.id, req.body.blacklist, req, res);
-    return res
-      .status(StatusCodes.OK)
-      .json(area);
-  }
+    this.logger.debug("updateArea request");
+    const updated_area = await AreaModel.updateOne(
+      { _id: req.params.areaId },
+      { $set: { [req.body.attr]: req.body.value } },
+    )
+      .then((data: any) => {
+        res.status(StatusCodes.OK).json(data);
+      })
+      .catch((err: any) => {
+        this.logger.error("Server internal error occurred!");
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(err);
+      });
+  };
 
   /**
    * Get single area by id
@@ -91,12 +125,148 @@ export class AreaController extends ResourceController<IArea>{
    * @param res
    */
   getAreaById = async (req: Request, res: Response) => {
-    this.logger.debug('getAreaById request');
+    this.logger.debug("getAreaById request");
     const area = await this.getOne(req.params.id, req, res);
-    return res
-      .status(StatusCodes.OK)
-      .json(area);
+    return res.status(StatusCodes.OK).json(area);
+  };
+
+  /**
+   * Initialize areas if the databaseis empty
+   */
+  private async checkAndInitialize() {
+    const area_data = await AreaModel.findOne({});
+    try {
+      if (!area_data) {
+        this.initAreas();
+      }
+    } catch (err) {
+      this.logger.error("Error ocurred in area initialization: ", err);
+    }
   }
+
+  initAreas = async () => {
+    const areaToInsert: Area_t[] = [
+      { name: "Computer Hardware", description: "No Description", image: "" },
+      { name: "Computer Networking", description: "No Description", image: "" },
+      { name: "Computer Software", description: "No Description", image: "" },
+      { name: "Cloud computing", description: "No Description", image: "" },
+      {
+        name: "Cyber Security and Ethical Hacking",
+        description: "No Description",
+        image: "",
+      },
+      {
+        name: "Data Science and Data Analysis",
+        description: "No Description",
+        image: "",
+      },
+      {
+        name: "Programming Language",
+        description: "No Description",
+        image: "",
+      },
+      { name: "Micro Architecture", description: "No Description", image: "" },
+      { name: "Operating system", description: "No Description", image: "" },
+      { name: "Web Development", description: "No Description", image: "" },
+      { name: "Web Designing", description: "No Description", image: "" },
+      { name: "Graphics design", description: "No Description", image: "" },
+      {
+        name: "Network Analytics and testing",
+        description: "No Description",
+        image: "",
+      },
+      { name: "Robotics", description: "No Description", image: "" },
+      {
+        name: "Artificial intelligence",
+        description: "No Description",
+        image: "",
+      },
+      {
+        name: "Computer Aided Design drafter",
+        description: "No Description",
+        image: "",
+      },
+      { name: "Data Entry Operator", description: "No Description", image: "" },
+      {
+        name: "DataBase management system",
+        description: "No Description",
+        image: "",
+      },
+      { name: "Video Game Designer", description: "No Description", image: "" },
+      {
+        name: "Computer Architecture and Engineering",
+        description: "No Description",
+        image: "",
+      },
+      { name: "UI designer", description: "No Description", image: "" },
+      {
+        name: "Computer Animation and 3D design",
+        description: "No Description",
+        image: "",
+      },
+      { name: "Computer vfx", description: "No Description", image: "" },
+      {
+        name: "Motion graphics and Visual Effects",
+        description: "No Description",
+        image: "",
+      },
+      { name: "Computer Biosystem", description: "No Description", image: "" },
+      { name: "Numeric analysis", description: "No Description", image: "" },
+      { name: "Cryptography", description: "No Description", image: "" },
+      {
+        name: "Research and development",
+        description: "No Description",
+        image: "",
+      },
+      {
+        name: "Traffic control system",
+        description: "No Description",
+        image: "",
+      },
+      {
+        name: "Mobile Application Development",
+        description: "No Description",
+        image: "",
+      },
+      { name: "Computer Accounting", description: "No Description", image: "" },
+      {
+        name: "User Experience design",
+        description: "No Description",
+        image: "",
+      },
+      { name: "SEO", description: "No Description", image: "" },
+      { name: "Digital Marketing", description: "No Description", image: "" },
+      { name: "Business Management", description: "No Description", image: "" },
+      {
+        name: "Stenographer and typist",
+        description: "No Description",
+        image: "",
+      },
+      {
+        name: "Clinical image processing",
+        description: "No Description",
+        image: "",
+      },
+      {
+        name: "Medical Record Technology",
+        description: "No Description",
+        image: "",
+      },
+      {
+        name: "Video editing and compositing",
+        description: "No Description",
+        image: "",
+      },
+    ];
+
+    await AreaModel.insertMany(areaToInsert)
+      .then((docs) => {
+        this.logger.success("Areas initialized successfully!");
+      })
+      .catch((err: any) => {
+        this.logger.error(err);
+      });
+  };
 
   /**
    * Initialize items
@@ -104,165 +274,120 @@ export class AreaController extends ResourceController<IArea>{
    * @param res
    */
   initializeArea = async (req: Request, res: Response) => {
-    this.logger.debug('Initialize area request');
+    this.logger.debug("Initialize area request");
 
     const areaToInsert: Area_t[] = [
-      { name: "Computer Hardware",
+      { name: "Computer Hardware", description: "No Description", image: "" },
+      { name: "Computer Networking", description: "No Description", image: "" },
+      { name: "Computer Software", description: "No Description", image: "" },
+      { name: "Cloud computing", description: "No Description", image: "" },
+      {
+        name: "Cyber Security and Ethical Hacking",
         description: "No Description",
-        image: "" 
+        image: "",
       },
-      { name: "Computer Networking",
+      {
+        name: "Data Science and Data Analysis",
         description: "No Description",
-        image: "" 
+        image: "",
       },
-      { name: "Computer Software",
+      {
+        name: "Programming Language",
         description: "No Description",
-        image: "" 
+        image: "",
       },
-      { name: "Cloud computing",
+      { name: "Micro Architecture", description: "No Description", image: "" },
+      { name: "Operating system", description: "No Description", image: "" },
+      { name: "Web Development", description: "No Description", image: "" },
+      { name: "Web Designing", description: "No Description", image: "" },
+      { name: "Graphics design", description: "No Description", image: "" },
+      {
+        name: "Network Analytics and testing",
         description: "No Description",
-        image: "" 
+        image: "",
       },
-      { name: "Cyber Security and Ethical Hacking",
+      { name: "Robotics", description: "No Description", image: "" },
+      {
+        name: "Artificial intelligence",
         description: "No Description",
-        image: "" 
+        image: "",
       },
-      { name: "Data Science and Data Analysis",
+      {
+        name: "Computer Aided Design drafter",
         description: "No Description",
-        image: "" 
+        image: "",
       },
-      { name: "Programming Language",
+      { name: "Data Entry Operator", description: "No Description", image: "" },
+      {
+        name: "DataBase management system",
         description: "No Description",
-        image: "" 
+        image: "",
       },
-      { name: "Micro Architecture",
+      { name: "Video Game Designer", description: "No Description", image: "" },
+      {
+        name: "Computer Architecture and Engineering",
         description: "No Description",
-        image: "" 
+        image: "",
       },
-      { name: "Operating system",
+      { name: "UI designer", description: "No Description", image: "" },
+      {
+        name: "Computer Animation and 3D design",
         description: "No Description",
-        image: "" 
+        image: "",
       },
-      { name: "Web Development",
+      { name: "Computer vfx", description: "No Description", image: "" },
+      {
+        name: "Motion graphics and Visual Effects",
         description: "No Description",
-        image: "" 
+        image: "",
       },
-      { name: "Web Designing",
+      { name: "Computer Biosystem", description: "No Description", image: "" },
+      { name: "Numeric analysis", description: "No Description", image: "" },
+      { name: "Cryptography", description: "No Description", image: "" },
+      {
+        name: "Research and development",
         description: "No Description",
-        image: "" 
+        image: "",
       },
-      { name: "Graphics design",
+      {
+        name: "Traffic control system",
         description: "No Description",
-        image: "" 
+        image: "",
       },
-      { name: "Network Analytics and testing",
+      {
+        name: "Mobile Application Development",
         description: "No Description",
-        image: "" 
+        image: "",
       },
-      { name: "Robotics",
+      { name: "Computer Accounting", description: "No Description", image: "" },
+      {
+        name: "User Experience design",
         description: "No Description",
-        image: "" 
+        image: "",
       },
-      { name: "Artificial intelligence",
+      { name: "SEO", description: "No Description", image: "" },
+      { name: "Digital Marketing", description: "No Description", image: "" },
+      { name: "Business Management", description: "No Description", image: "" },
+      {
+        name: "Stenographer and typist",
         description: "No Description",
-        image: "" 
+        image: "",
       },
-      { name: "Computer Aided Design drafter",
+      {
+        name: "Clinical image processing",
         description: "No Description",
-        image: "" 
+        image: "",
       },
-      { name: "Data Entry Operator",
+      {
+        name: "Medical Record Technology",
         description: "No Description",
-        image: "" 
+        image: "",
       },
-      { name: "DataBase management system",
+      {
+        name: "Video editing and compositing",
         description: "No Description",
-        image: "" 
+        image: "",
       },
-      { name: "Video Game Designer",
-        description: "No Description",
-        image: "" 
-      },
-      { name: "Computer Architecture and Engineering",
-        description: "No Description",
-        image: "" 
-      },
-      { name: "UI designer",
-        description: "No Description",
-        image: "" 
-      },
-      { name: "Computer Animation and 3D design",
-        description: "No Description",
-        image: "" 
-      },
-      { name: "Computer vfx",
-        description: "No Description",
-        image: "" 
-      },
-      { name: "Motion graphics and Visual Effects",
-        description: "No Description",
-        image: "" 
-      },
-      { name: "Computer Biosystem",
-        description: "No Description",
-        image: "" 
-      },
-      { name: "Numeric analysis",
-        description: "No Description",
-        image: "" 
-      },
-      { name: "Cryptography",
-        description: "No Description",
-        image: "" 
-      },
-      { name: "Research and development",
-        description: "No Description",
-        image: "" 
-      },
-      { name: "Traffic control system",
-        description: "No Description",
-        image: "" 
-      },
-      { name: "Mobile Application Development",
-        description: "No Description",
-        image: "" 
-      },
-      { name: "Computer Accounting",
-        description: "No Description",
-        image: "" 
-      },
-      { name: "User Experience design",
-        description: "No Description",
-        image: "" 
-      },
-      { name: "SEO",
-        description: "No Description",
-        image: "" 
-      },
-      { name: "Digital Marketing",
-        description: "No Description",
-        image: "" 
-      },
-      { name: "Business Management",
-        description: "No Description",
-        image: "" 
-      },
-      { name: "Stenographer and typist",
-        description: "No Description",
-        image: "" 
-      },
-      { name: "Clinical image processing",
-        description: "No Description",
-        image: "" 
-      },
-      { name: "Medical Record Technology",
-        description: "No Description",
-        image: "" 
-      },
-      { name: "Video editing and compositing",
-        description: "No Description",
-        image: "" 
-      }
     ];
 
     await AreaModel.insertMany(areaToInsert)
@@ -270,8 +395,7 @@ export class AreaController extends ResourceController<IArea>{
         res.json(docs);
       })
       .catch((err) => {
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR)
-      })
-
-  }
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR);
+      });
+  };
 }
