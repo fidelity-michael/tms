d12fca3f49171e4e39695a2ceff4b45785c53de4
Michael <mixkok2001@gmail.com>
2024-11-16 20:13:42 +0200

    Added publishTo(...) function that emits events that are registered in the frontend from the backend

--- tms-backend/src/services/sockets/sockets-service.ts
+++ tms-backend/src/services/sockets/sockets-service.ts
@@ -1,16 +1,12 @@
 import "reflect-metadata";
-import http from 'http';
-import { injectable } from 'inversify';
-import { SocketServer } from './socket-server';
+import http from "http";
+import { injectable } from "inversify";
+import { SocketServer } from "./socket-server";
 
-
-// WARN: Possibly breaking?
-// @injectable()
 export class SocketsService {
+  private socketServer!: SocketServer;
 
-  public socketServer!: SocketServer;
-
-  constructor() { }
+  constructor() {}
 
   /**
    * Init and start socket server
@@ -19,18 +15,39 @@ export class SocketsService {
    * @returns
    */
   public async start(server: http.Server) {
-    if (this.socketServer) { return; }
+    if (this.socketServer) {
+      return;
+    }
 
     this.socketServer = new SocketServer();
     await this.socketServer.start(server);
   }
 
+  /**
+   * Emits an event that's registered in the frontend
+   *
+   * @param {string} event name of the event
+   * @param {any} data data to be sent
+   */
   public publish(event: string, data: any) {
-    /** 
+    /**
      * "server:event" is predefined channel for every server event.
      * Every socket-client in the frontend has subscribed in this event
-    */
+     */
     this.socketServer.io.emit(event, data);
   }
 
+  /**
+   * Emits an event that's registered in the frontend
+   *
+   * Consider to prefix the event with its namespace on the event variable. For
+   * example if the namespace name is "admin", then the event name should be "admin:eventName"
+   * @param {string} event name of the event
+   * @param {string} receiver room name or SocketId (used for private messaging)
+   * @param {string} namespace namespace name
+   *
+   */
+  public publishTo(namespace: string, receiver: string, event: string) {
+    this.socketServer.io.of(namespace).to(receiver).emit(event);
+  }
 }
