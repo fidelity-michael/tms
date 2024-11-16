e692e27c76844438c16129a288aa029abb996e54
Michael <mixkok2001@gmail.com>
2024-11-16 20:14:56 +0200

    Made userSocket variable private and added getter function. Also socketuser key-value pair isn't overwritten if it already exists now

--- tms-backend/src/services/sockets/socket-server/user-sockets.ts
+++ tms-backend/src/services/sockets/socket-server/user-sockets.ts
@@ -1,3 +1,4 @@
+import { Logger } from "../../../api/shared/utils/logger";
 /**
  * Interface for user sockets type.
  * Key: socketId, Value: userId
@@ -7,35 +8,44 @@ export interface UserSockets {
 }
 
 export class UserSocketStore {
+  private logger = Logger.getInstance();
   private static instance: UserSocketStore;
-  public userSockets: UserSockets = {};
+  private userSockets: UserSockets = {};
 
-  private constructor(){}
+  private constructor() {}
 
   /**
-   * Returns the UserSocketStore instance 
+   * Returns the UserSocketStore instance
    * @returns {UserSocketStore} class instance
    */
   static getInstance(): UserSocketStore {
-    if(!UserSocketStore.instance)
+    if (!UserSocketStore.instance)
       UserSocketStore.instance = new UserSocketStore();
     return UserSocketStore.instance;
   }
 
-  /**
-  * Adds key-value pair for socket to user pairing
-  * @param userId {string} user id
-  * @param socketId {string} socket id
-  */
-  addUserSocket(socketId: string, userId: string){
-    this.userSockets[socketId] = userId;
+  public getUserSockets(): UserSockets {
+    return this.userSockets;
   }
 
   /**
-  * Remove key-value pair of socketId to user 
-  * @param socketId {string} socket id
-  */
-  removeUserSocket(socketId: string){
+   * Adds key-value pair for socket to user pairing
+   * @param userId {string} user id
+   * @param socketId {string} socket id
+   */
+  addUserSocket(socketId: string, userId: string) {
+    if (!this.userSockets[socketId]) this.userSockets[socketId] = userId;
+    else
+      this.logger.debug(
+        `Socket ID ${socketId} already exists with user ID ${this.userSockets[socketId]}`,
+      );
+  }
+
+  /**
+   * Remove key-value pair of socketId to user
+   * @param socketId {string} socket id
+   */
+  removeUserSocket(socketId: string) {
     delete this.userSockets[socketId];
   }
 }
