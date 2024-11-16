86f731ba1898bf10160b19886da1b556e6bb654f
Michael <mixkok2001@gmail.com>
2024-11-17 01:54:20 +0200

    Fixed cleanup code that impedes socket.io from working properly

--- tms-frontend/src/components/navbar/Sidebar/ProfessorSidebar.jsx
+++ tms-frontend/src/components/navbar/Sidebar/ProfessorSidebar.jsx
@@ -1,100 +1,110 @@
 import React, { useEffect, useRef, useState } from "react";
-import io from 'socket.io-client';
-import axios from 'axios';
-import './sidebar.css'
+import io from "socket.io-client";
+import axios from "axios";
+import "./sidebar.css";
 
 function ProfessorSidebar(props) {
-
-  const [selected, setSelected] = useState('');  
+  const [selected, setSelected] = useState("");
 
   //for sockets
-  const socketRef = useRef(null);  
-  const ENDPOINT = 'http://localhost:8080/chat';
+  const socketRef = useRef(null);
+  const ENDPOINT = "http://localhost:8080/chat";
 
   //we establish connection with endpoint
   useEffect(() => {
-
-    function notifyChatIcon(){
-      if(selected !== "Chat")
-        document.getElementById("chatCircle").style = "display: block !important"
+    function notifyChatIcon() {
+      if (selected !== "Chat")
+        document.getElementById("chatCircle").style =
+          "display: block !important";
     }
 
     //check if there are unread messages
     const checkUnread = async () => {
-      await axios.get('/chat/privateConversation/'+props.userId)
-      .then((res) => {
-        console.log(res.data, selected)
+      await axios
+        .get("/chat/privateConversation/" + props.userId)
+        .then((res) => {
+          console.log(res.data, selected);
 
-        if(selected !== "Chat"){
-          for(var i=0; i<res.data.length; i++){
-            if(res.data[i].lastMessage.sender!==props.userId && res.data[i].lastMessage.read.length===0){
-              notifyChatIcon()
-              break;
-            } 
+          if (selected !== "Chat") {
+            for (var i = 0; i < res.data.length; i++) {
+              if (
+                res.data[i].lastMessage.sender !== props.userId &&
+                res.data[i].lastMessage.read.length === 0
+              ) {
+                notifyChatIcon();
+                break;
+              }
+            }
           }
-        }
-        
-      })
-      .catch(() => {
-        console.log("Errooor")
-      })
-    }
+        })
+        .catch(() => {
+          console.log("Errooor");
+        });
+    };
 
-    if(props.userId){
-
-      if (socketRef.current == null) { //current will persist for the full lifetime of the component
+    if (props.userId) {
+      if (socketRef.current == null) {
+        //current will persist for the full lifetime of the component
         socketRef.current = io(ENDPOINT);
       }
 
-      checkUnread()
+      checkUnread();
 
       socketRef.current.on("connect", () => {
-        console.log('Sidebar connected to chat server!')
-        socketRef.current.emit('map', props.userId);
-      })
+        console.log("Sidebar connected to chat server!");
+        socketRef.current.emit("chat:map", props.userId);
+      });
 
       //cleanup (disconnect from chat server)
-      return () => { 
-        if(socketRef.current){
-            socketRef.current.disconnect()
-            socketRef.current.close()
-        }
-      
-      };
+      // return () => {
+      //   if (socketRef.current) {
+      //     socketRef.current.disconnect();
+      //     socketRef.current.close();
+      //   }
+      // };
     }
-    
-  }, [ENDPOINT, props.userId])
-
+  }, [ENDPOINT, props.userId]);
+  // TODO: Fix emits and on events
   useEffect(() => {
-    function notifyChatIcon(){
-      if(selected !== "Chat")
-        document.getElementById("chatCircle").style = "display: block !important"
+    function notifyChatIcon() {
+      if (selected !== "Chat")
+        document.getElementById("chatCircle").style =
+          "display: block !important";
     }
 
-    if(socketRef.current)
-      socketRef.current.on("privateMessage", () => {notifyChatIcon()})   
+    if (socketRef.current)
+      socketRef.current.on("chat:privateMessage", () => {
+        notifyChatIcon();
+      });
 
     //cleanup events
-    return() => {
-      socketRef.current.off("privateMessage") 
-    }
-  }, [selected, socketRef.current])
+    // return() => {
+    //   socketRef.current.off("chat:privateMessage")
+    // }
+  }, [selected, socketRef.current]);
 
-  function hideNotifyChatIcon(){
-    document.getElementById("chatCircle").style = "display: none !important"
+  function hideNotifyChatIcon() {
+    document.getElementById("chatCircle").style = "display: none !important";
   }
 
-
   return (
-    <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+    <ul
+      className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion"
+      id="accordionSidebar"
+    >
       <div className="sidebar-brand d-flex" href="#/">
-        <i className="fas fa-user-tie" style={{marginTop: "0.2rem"}}></i>
-        <div style={{marginLeft: "0.7rem"}}>Professor</div>
+        <i className="fas fa-user-tie" style={{ marginTop: "0.2rem" }}></i>
+        <div style={{ marginLeft: "0.7rem" }}>Professor</div>
       </div>
 
-      <li className="nav-item" onClick={(e) => { props.onSelect("Dashboard"); setSelected("Dashboard");}}>
-        <div className="nav-link" style={{cursor: "pointer"}} >
-
+      <li
+        className="nav-item"
+        onClick={(e) => {
+          props.onSelect("Dashboard");
+          setSelected("Dashboard");
+        }}
+      >
+        <div className="nav-link" style={{ cursor: "pointer" }}>
           <div className="imgSidebar">
             <i className="fas fa-fw fa-home"></i>
           </div>
@@ -103,9 +113,14 @@ function ProfessorSidebar(props) {
         </div>
       </li>
 
-      <li className="nav-item" onClick={(e) => { props.onSelect("New Thesis"); setSelected("New Thesis");}}>
-        <div className="nav-link" style={{cursor: "pointer"}} >
-
+      <li
+        className="nav-item"
+        onClick={(e) => {
+          props.onSelect("New Thesis");
+          setSelected("New Thesis");
+        }}
+      >
+        <div className="nav-link" style={{ cursor: "pointer" }}>
           <div className="imgSidebar">
             <i className="fa fa-fw fa-folder-plus"></i>
           </div>
@@ -114,31 +129,46 @@ function ProfessorSidebar(props) {
         </div>
       </li>
 
-      <li className="nav-item" onClick={(e) => { props.onSelect("Assigned Theses"); setSelected("Assigned Theses");}}>
-        <div className="nav-link" style={{cursor: "pointer"}} >
-
+      <li
+        className="nav-item"
+        onClick={(e) => {
+          props.onSelect("Assigned Theses");
+          setSelected("Assigned Theses");
+        }}
+      >
+        <div className="nav-link" style={{ cursor: "pointer" }}>
           <div className="imgSidebar">
             <i className="fas fa-fw fa-folder-open"></i>
           </div>
-        
+
           <span>Assigned Theses</span>
         </div>
       </li>
 
-      <li className="nav-item" onClick={(e) => { props.onSelect("Supervise Theses"); setSelected("Supervise Theses");}}>
-        <div className="nav-link" style={{cursor: "pointer"}} >
-
+      <li
+        className="nav-item"
+        onClick={(e) => {
+          props.onSelect("Supervise Theses");
+          setSelected("Supervise Theses");
+        }}
+      >
+        <div className="nav-link" style={{ cursor: "pointer" }}>
           <div className="imgSidebar">
             <i className="fas fa-fw fa-glasses"></i>
           </div>
-        
+
           <span>Supervise Theses</span>
         </div>
       </li>
 
-      <li className="nav-item" onClick={(e) => { props.onSelect("Theses Requests"); setSelected("Theses Requests");}}>
-        <div className="nav-link" style={{cursor: "pointer"}} >
-
+      <li
+        className="nav-item"
+        onClick={(e) => {
+          props.onSelect("Theses Requests");
+          setSelected("Theses Requests");
+        }}
+      >
+        <div className="nav-link" style={{ cursor: "pointer" }}>
           <div className="imgSidebar">
             <i className="fas fa-fw fa-envelope"></i>
           </div>
@@ -147,9 +177,14 @@ function ProfessorSidebar(props) {
         </div>
       </li>
 
-      <li className="nav-item" onClick={(e) => { props.onSelect("Theses Archive"); setSelected("Theses Archive");}}>
-        <div className="nav-link" style={{cursor: "pointer"}} >
-
+      <li
+        className="nav-item"
+        onClick={(e) => {
+          props.onSelect("Theses Archive");
+          setSelected("Theses Archive");
+        }}
+      >
+        <div className="nav-link" style={{ cursor: "pointer" }}>
           <div className="imgSidebar">
             <i className="fas fa-fw fa-archive"></i>
           </div>
@@ -158,9 +193,15 @@ function ProfessorSidebar(props) {
         </div>
       </li>
 
-      <li className="nav-item" onClick={(e) => { props.onSelect("Chat"); hideNotifyChatIcon(); setSelected("Chat");}}>
-        <div className="nav-link" style={{cursor: "pointer"}} >
-
+      <li
+        className="nav-item"
+        onClick={(e) => {
+          props.onSelect("Chat");
+          hideNotifyChatIcon();
+          setSelected("Chat");
+        }}
+      >
+        <div className="nav-link" style={{ cursor: "pointer" }}>
           <div className="imgSidebar">
             <i className="far fa-comments"></i>
           </div>
@@ -169,7 +210,6 @@ function ProfessorSidebar(props) {
           <i className="fas fa-circle chatNotification" id="chatCircle"></i>
         </div>
       </li>
-
     </ul>
   );
 }
--- tms-frontend/src/components/navbar/Sidebar/StudentSidebar.jsx
+++ tms-frontend/src/components/navbar/Sidebar/StudentSidebar.jsx
@@ -1,108 +1,133 @@
 import { useEffect, useRef, useState } from "react";
-import io from 'socket.io-client';
-import axios from 'axios';
+import io from "socket.io-client";
+import axios from "axios";
 
 function StudentSidebar(props) {
-
-  const [selected, setSelected] = useState('');  
+  const [selected, setSelected] = useState("");
 
   //for sockets
-  const socketRef = useRef(null);  
-  const ENDPOINT = 'http://localhost:8080/chat';
+  const socketRef = useRef(null);
+  const ENDPOINT = "http://localhost:8080/chat";
 
   //we establish connection with endpoint
-  useEffect(() => {   
-
-    function notifyChatIcon(){
-      if(selected !== "Chat")
-        document.getElementById("chatCircle").style = "display: block !important"
+  useEffect(() => {
+    function notifyChatIcon() {
+      if (selected !== "Chat")
+        document.getElementById("chatCircle").style =
+          "display: block !important";
     }
 
     //check if there are unread messages
     const checkUnread = async () => {
-      await axios.get('/chat/privateConversation/'+props.userId)
-      .then((res) => {
-        console.log(res.data)
+      await axios
+        .get("/chat/privateConversation/" + props.userId)
+        .then((res) => {
+          console.log(res.data);
 
-        if(props.page !== "Chat"){
-          for(var i=0; i<res.data.length; i++){
-            if(res.data[i].lastMessage.sender!==props.userId && res.data[i].lastMessage.read.length===0){
-              notifyChatIcon()
-              break;
-            } 
+          if (props.page !== "Chat") {
+            for (var i = 0; i < res.data.length; i++) {
+              if (
+                res.data[i].lastMessage.sender !== props.userId &&
+                res.data[i].lastMessage.read.length === 0
+              ) {
+                notifyChatIcon();
+                break;
+              }
+            }
           }
-        }
-        
-      })
-      .catch(() => {
-        console.log("Errooor")
-      })
-    }
+        })
+        .catch(() => {
+          console.log("Errooor");
+        });
+    };
 
-    if(props.userId){
-      if (socketRef.current == null) { //current will persist for the full lifetime of the component
+    if (props.userId) {
+      if (socketRef.current == null) {
+        //current will persist for the full lifetime of the component
         socketRef.current = io(ENDPOINT);
       }
 
-      checkUnread()
+      checkUnread();
 
       socketRef.current.on("connect", () => {
-        console.log('Sidebar connected to chat server!')
-        socketRef.current.emit('map', props.userId);
-      })
+        console.log("Sidebar connected to chat server!");
+        socketRef.current.emit("chat:map", props.userId);
+      });
 
       //cleanup (disconnect from chat server)
-      return () => { 
-        if(socketRef.current){
-            socketRef.current.disconnect()
-            socketRef.current.close()
-        }
-      
-      };
+      // return () => {
+      //   if (socketRef.current) {
+      //     socketRef.current.disconnect();
+      //     socketRef.current.close();
+      //   }
+      // };
     }
-    
-  }, [ENDPOINT, props.userId])
+  }, [ENDPOINT, props.userId]);
 
   useEffect(() => {
-    function notifyChatIcon(){
-      if(selected !== "Chat")
-        document.getElementById("chatCircle").style = "display: block !important"
+    function notifyChatIcon() {
+      if (selected !== "Chat")
+        document.getElementById("chatCircle").style =
+          "display: block !important";
     }
 
-    if(socketRef.current)
-      socketRef.current.on("privateMessage", () => {notifyChatIcon()})   
+    // TODO: Fix emits and on events
+    if (socketRef.current)
+      socketRef.current.on("chat:privateMessage", () => {
+        notifyChatIcon();
+      });
 
     //cleanup events
-    return() => {
-      socketRef.current.off("privateMessage") 
-    }
-  }, [selected, socketRef.current])
+    return () => {
+      // socketRef.current.off("chat:privateMessage")
+    };
+  }, [selected, socketRef.current]);
 
-  function hideNotifyChatIcon(){
-    document.getElementById("chatCircle").style = "display: none !important"
+  function hideNotifyChatIcon() {
+    document.getElementById("chatCircle").style = "display: none !important";
   }
 
   return (
-    <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-
+    <ul
+      className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion"
+      id="accordionSidebar"
+    >
       <div className="sidebar-brand d-flex " href="#/">
-        <i className="fas fa-graduation-cap" style={{marginTop: "0.25rem"}}></i>
-        <div id="home" style={{marginLeft: "0.3rem"}}>Student</div>
+        <i
+          className="fas fa-graduation-cap"
+          style={{ marginTop: "0.25rem" }}
+        ></i>
+        <div id="home" style={{ marginLeft: "0.3rem" }}>
+          Student
+        </div>
       </div>
 
-      <li className="nav-item side" id="Dashboard" onClick={(e) => { props.onSelect("Dashboard"); setSelected("Dashboard");}}>
-        <div className="nav-link" style={{cursor: "pointer"}} >
+      <li
+        className="nav-item side"
+        id="Dashboard"
+        onClick={(e) => {
+          props.onSelect("Dashboard");
+          setSelected("Dashboard");
+        }}
+      >
+        <div className="nav-link" style={{ cursor: "pointer" }}>
           <div className="imgSidebar">
             <i className="fas fa-fw fa-home"></i>
           </div>
-          
+
           <span className="spanSidebar">Dashboard</span>
-          
         </div>
       </li>
 
-      <li className="nav-item side" id="Favourite Areas" onClick={(e) => { props.onSelect("Favourite Areas"); setSelected("Favourite Areas")}} >
-        <div className="nav-link" style={{cursor: "pointer"}} >
+      <li
+        className="nav-item side"
+        id="Favourite Areas"
+        onClick={(e) => {
+          props.onSelect("Favourite Areas");
+          setSelected("Favourite Areas");
+        }}
+      >
+        <div className="nav-link" style={{ cursor: "pointer" }}>
           <div className="imgSidebar">
             <i className="far fa-fw fa-heart"></i>
           </div>
@@ -111,8 +136,15 @@ function StudentSidebar(props) {
         </div>
       </li>
 
-      <li className="nav-item side" id="Favourite Areas" onClick={(e) => { props.onSelect("Available Theses"); setSelected("Available Theses")}} >
-        <div className="nav-link" style={{cursor: "pointer"}} >
+      <li
+        className="nav-item side"
+        id="Favourite Areas"
+        onClick={(e) => {
+          props.onSelect("Available Theses");
+          setSelected("Available Theses");
+        }}
+      >
+        <div className="nav-link" style={{ cursor: "pointer" }}>
           <div className="imgSidebar">
             <i className="fas fa-fw fa-clipboard-list"></i>
           </div>
@@ -121,8 +153,15 @@ function StudentSidebar(props) {
         </div>
       </li>
 
-      <li className="nav-item side" id="Requests Approved" onClick={(e) => { props.onSelect("Requests Approved"); setSelected("Requests Approved")}} >
-        <div className="nav-link" style={{cursor: "pointer"}} >
+      <li
+        className="nav-item side"
+        id="Requests Approved"
+        onClick={(e) => {
+          props.onSelect("Requests Approved");
+          setSelected("Requests Approved");
+        }}
+      >
+        <div className="nav-link" style={{ cursor: "pointer" }}>
           <div className="imgSidebar">
             <i className="fa fa-fw fa-clipboard-check"></i>
           </div>
@@ -131,37 +170,57 @@ function StudentSidebar(props) {
         </div>
       </li>
 
-      <li className="nav-item side" id="My Thesis" onClick={(e) => { props.onSelect("My Thesis"); setSelected("My Thesis")}} >
-        <div className="nav-link" style={{cursor: "pointer"}} >
+      <li
+        className="nav-item side"
+        id="My Thesis"
+        onClick={(e) => {
+          props.onSelect("My Thesis");
+          setSelected("My Thesis");
+        }}
+      >
+        <div className="nav-link" style={{ cursor: "pointer" }}>
           <div className="imgSidebar">
             <i className="fa fa-fw fa-folder-open"></i>
           </div>
-          
+
           <span className="spanSidebar">My Thesis</span>
         </div>
       </li>
 
-      <li className="nav-item side" id="My Reports" onClick={(e) => { props.onSelect("My Reports"); setSelected("My Reports")}} >
-        <div className="nav-link" style={{cursor: "pointer"}} >
+      <li
+        className="nav-item side"
+        id="My Reports"
+        onClick={(e) => {
+          props.onSelect("My Reports");
+          setSelected("My Reports");
+        }}
+      >
+        <div className="nav-link" style={{ cursor: "pointer" }}>
           <div className="imgSidebar">
             <i className="far fa-clipboard"></i>
           </div>
-          
+
           <span className="spanSidebar">My Reports</span>
         </div>
       </li>
 
-      <li className="nav-item side" onClick={(e) => { props.onSelect("Chat"); hideNotifyChatIcon(); setSelected("Chat")}}>
-        <div className="nav-link" style={{cursor: "pointer"}} >
+      <li
+        className="nav-item side"
+        onClick={(e) => {
+          props.onSelect("Chat");
+          hideNotifyChatIcon();
+          setSelected("Chat");
+        }}
+      >
+        <div className="nav-link" style={{ cursor: "pointer" }}>
           <div className="imgSidebar">
             <i className="far fa-comments"></i>
           </div>
-          
+
           <span className="spanSidebar">Chat</span>
           <i className="fas fa-circle chatNotification" id="chatCircle"></i>
         </div>
       </li>
-
     </ul>
   );
 }
