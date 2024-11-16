029b301eb307c401daaa63478f39d9ae8b1da906
Michael <mixkok2001@gmail.com>
2024-11-16 20:25:13 +0200

    Small changed in postThesisReport that now creates a ThesesRepModel object

--- tms-backend/src/api/v1/theses_reports/theses_rep-controller.ts
+++ tms-backend/src/api/v1/theses_reports/theses_rep-controller.ts
@@ -3,6 +3,7 @@ import { IThesisReport, ThesesRepModel, ThesesRep_t } from "./theses_rep-model";
 import { ResourceController } from "../../shared";
 import { StatusCodes } from "http-status-codes";
 import { Logger } from "../../shared/utils/logger";
+import { ThesisModel } from "../theses/theses-model";
 
 export class ThesesRepController extends ResourceController<IThesisReport> {
   private logger: Logger = Logger.getInstance();
@@ -30,8 +31,23 @@ export class ThesesRepController extends ResourceController<IThesisReport> {
    */
   postThesisReport = async (req: Request, res: Response) => {
     this.logger.debug("postThesisReport request");
-    const thesisReport = await this.create(req, res);
-    return res.status(StatusCodes.OK).json({ report: thesisReport._id });
+    // Create a new Thesis Report
+    try {
+      const report = new ThesesRepModel({
+        title: req.body.title,
+        description: req.body.description,
+        isFinal: req.body.isFinal,
+        student: req.body.userId,
+        report_files: req.body.report_files,
+        date: Date.now(),
+      });
+
+      const saved_report = await report.save();
+      res.send({ report: report._id });
+    } catch (err) {
+      // console.log(err);
+      res.status(StatusCodes.BAD_REQUEST).send(err);
+    }
   };
 
   /**
