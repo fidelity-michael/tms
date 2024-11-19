e64691767fb55a5274b7eccaad5e88b00a892167
Michael <mixkok2001@gmail.com>
2024-11-19 20:01:02 +0200

    Reverted back code in LoginPage for handling resizing and total size of LoginForm

--- tms-frontend/src/webpages/LoginPage/LoginPage.jsx
+++ tms-frontend/src/webpages/LoginPage/LoginPage.jsx
@@ -5,16 +5,16 @@ import './style.css';
 function LoginPage() {
     
     return (
-        <div className='tw-flex tw-items-start tw-justify-center tw-h-screen tw-font-dm container login-container'>
+        <div className='container login-container'>
             <div className='row'>
-                <div className='col-md-6 tw-bg-dark-sky-blue tw-overflow-auto tw-p-8 tw-shadow-2xl'>
-                    <div className='tw-left-2 tw-mt-8 tw-ml-9 tw-mb-2 tw-text-light-pale-blue-white'>
+                <div className='col-md-6 thesis-form'>
+                    <div className='thesis-header'>
                         <span className='welcome-lbl'>Welcome to </span> <br/>
                         <span className='thesis-lbl'>Thesis Management <br/> System</span>
                     </div>
                 </div>
-                <div className='col-md-6 tw-overflow-auto tw-pb-16 tw-bg-light-pale-blue-white tw-shadow-2xl'>
-                    <h3 className='tw-mt-16 tw-ml-8 tw-font-bold tw-left-2 tw-text-mid-pale-blue tw-text-4xl'>Login</h3>
+                <div className='col-md-6 login-form'>
+                    <h3>Login</h3>
                     <LoginForm/>
                 </div>
             </div>
