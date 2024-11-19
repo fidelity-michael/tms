e2a245d84a2f48c0236395bc3e928985c5052cb2
Michael <mixkok2001@gmail.com>
2024-11-19 20:31:55 +0200

    Added the tms owl logo to the login page. The resizing remains correct

--- tms-frontend/src/webpages/LoginPage/LoginPage.jsx
+++ tms-frontend/src/webpages/LoginPage/LoginPage.jsx
@@ -1,25 +1,33 @@
-import LoginForm from '../../components/forms/LoginForm/LoginForm';
+import LoginForm from "../../components/forms/LoginForm/LoginForm";
 import "mdbreact/dist/css/mdb.css";
-import './style.css';
+import "./style.css";
 
 function LoginPage() {
-    
-    return (
-        <div className='container login-container'>
-            <div className='row'>
-                <div className='col-md-6 thesis-form'>
-                    <div className='thesis-header'>
-                        <span className='welcome-lbl'>Welcome to </span> <br/>
-                        <span className='thesis-lbl'>Thesis Management <br/> System</span>
-                    </div>
-                </div>
-                <div className='col-md-6 login-form'>
-                    <h3>Login</h3>
-                    <LoginForm/>
-                </div>
+  return (
+    <div className="container login-container">
+      <div className="row">
+          <div className="col-md-6 thesis-form lg:tw-pr-16">
+            <div className="tw-flex tw-justify-center lg:tw-mt-8">
+              <img
+                className="tw-object-scale-down tw-h-32 tw-w-78"
+                src="/public/logo.svg"
+                alt="logo"
+              />
             </div>
+            <div className="thesis-header">
+              <span className="welcome-lbl">Welcome to the</span> <br />
+              <span className="thesis-lbl">
+                Thesis Management <br /> System
+              </span>
+            </div>
+          </div>
+        <div className="col-md-6 login-form">
+          <h3>Login</h3>
+          <LoginForm />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default LoginPage;
