812792dcc9d8977f53dda8c8a0567f0575092515
Michael <mixkok2001@gmail.com>
2024-11-20 21:12:37 +0200

    Added options for admin sidebar

---
+++ tms-frontend/src/components/navbar/Sidebar/options.tsx
@@ -0,0 +1,51 @@
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import Group from "@mui/icons-material/Group";
+import AnalyticsIcon from '@mui/icons-material/Analytics';
+import SchoolIcon from '@mui/icons-material/School';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import ListAltIcon from '@mui/icons-material/ListAlt';
+import FolderIcon from '@mui/icons-material/Folder';
+import InventoryIcon from '@mui/icons-material/Inventory';
+
+export const AdminSidebarLinks = [
+  {
+    key: "dashboard",
+    label: "Dashboard",
+    icon: <DashboardIcon fontSize="large" className="tw-text-light-sky-blue" />,
+  },
+  {
+    key: "users",
+    label: "Users",
+    icon: <Group fontSize="large" className="tw-text-light-sky-blue" />,
+  },
+  {
+    key: "analytics",
+    label: "Statistics",
+    icon: <AnalyticsIcon fontSize="large" className="tw-text-light-sky-blue" />,
+  },
+  {
+    key: "universities",
+    label: "Universities",
+    icon: <SchoolIcon fontSize="large" className="tw-text-light-sky-blue" />,
+  },
+  {
+    key: "departments",
+    label: "Departments",
+    icon: <AccountBalanceIcon fontSize="large" className="tw-text-light-sky-blue" />,
+  },
+  {
+    key: "categories",
+    label: "Areas / Categories",
+    icon: <ListAltIcon fontSize="large" className="tw-text-light-sky-blue" />,
+  },
+  {
+    key: "assigned_theses",
+    label: "Assigned Theses",
+    icon: <FolderIcon fontSize="large" className="tw-text-light-sky-blue" />,
+  },
+  {
+    key: "archived_theses",
+    label: "Archived Theses",
+    icon: <InventoryIcon fontSize="large" className="tw-text-light-sky-blue" />,
+  },
+];
