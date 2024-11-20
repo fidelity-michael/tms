44381dc8c0738dfa24ea726ffbbd1025ad229a7d
Michael <mixkok2001@gmail.com>
2024-11-20 15:38:45 +0200

    Formatted LoginPage

--- tms-frontend/src/webpages/LoginPage/LoginPage.jsx
+++ tms-frontend/src/webpages/LoginPage/LoginPage.jsx
@@ -6,21 +6,21 @@ function LoginPage() {
   return (
     <div className="container login-container">
       <div className="row">
-          <div className="col-md-6 thesis-form lg:tw-pr-16">
-            <div className="tw-flex tw-justify-center lg:tw-mt-8">
-              <img
-                className="tw-object-scale-down tw-h-32 tw-w-78"
-                src="/public/logo.svg"
-                alt="logo"
-              />
-            </div>
-            <div className="thesis-header">
-              <span className="welcome-lbl">Welcome to the</span> <br />
-              <span className="thesis-lbl">
-                Thesis Management <br /> System
-              </span>
-            </div>
+        <div className="col-md-6 thesis-form lg:tw-pr-16">
+          <div className="tw-flex tw-justify-center lg:tw-mt-8">
+            <img
+              className="tw-object-scale-down tw-h-32 tw-w-78"
+              src="/public/logo.svg"
+              alt="logo"
+            />
           </div>
+          <div className="thesis-header">
+            <span className="welcome-lbl">Welcome to the</span> <br />
+            <span className="thesis-lbl">
+              Thesis Management <br /> System
+            </span>
+          </div>
+        </div>
         <div className="col-md-6 login-form">
           <h3>Login</h3>
           <LoginForm />
