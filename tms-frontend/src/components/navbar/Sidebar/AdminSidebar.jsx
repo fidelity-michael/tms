0dfb752101188102e0211ff06e55d9d053a76eb4
Michael <mixkok2001@gmail.com>
2024-11-26 16:05:16 +0200

    Added tailwind options in sidebar to enable the sidebar to remain fixed while the main content scrolls (on its right)

--- tms-frontend/src/components/navbar/Sidebar/AdminSidebar.jsx
+++ tms-frontend/src/components/navbar/Sidebar/AdminSidebar.jsx
@@ -11,7 +11,7 @@ export default function AdminSidebar(props) {
   }
 
   return (
-    <div className="tw-flex tw-flex-col tw-h-screen tw-py-5 tw-w-80">
+    <div className="tw-sticky tw-top-0 tw-flex tw-flex-col tw-h-screen tw-py-5 tw-w-80">
       <TopSidebar props={props} />
       <div className="tw-flex tw-flex-col tw-pl-1 tw-gap-6 tw-pt-8 tw-border-t-4 tw-border-light-pale-blue-white">
         {AdminSidebarLinks.map((item) => (
