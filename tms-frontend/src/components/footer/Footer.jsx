a0552e0870cbe99259b035e0de1abf59f0bf47a5
Michael <mixkok2001@gmail.com>
2024-11-27 02:33:40 +0200

    Updated footer background color and date

--- tms-frontend/src/components/footer/Footer.jsx
+++ tms-frontend/src/components/footer/Footer.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
 
 function Footer() {
     return (
-        <footer className="sticky-footer bg-white">
+        <footer className="sticky-footer tw-bg-light-pale-blue-white">
             <div className="container my-auto">
                 <div className="copyright text-center my-auto">
-                    <span>Copyright &copy;  2020 <a href='https://www.csd.uoc.gr/' target='_blank' rel="noopener noreferrer">Computer Science Department UoC</a></span>
+                    <span>Copyright &copy;  2024 <a href='https://www.csd.uoc.gr/' target='_blank' rel="noopener noreferrer">Computer Science Department UoC</a></span>
                 </div>
             </div>
         </footer>
