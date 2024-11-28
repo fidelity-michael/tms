1c656752194c6df3b0520bb8df48d2b45422001e
Michael <mixkok2001@gmail.com>
2024-11-28 15:57:31 +0200

    SmallTable fixed and centered data and rows

--- tms-frontend/src/components/content/SmallTable.tsx
+++ tms-frontend/src/components/content/SmallTable.tsx
@@ -9,7 +9,7 @@ interface UserData {
   role?: string[];
   first_name?: string;
   last_name?: string;
-  active?: boolean;
+  active?: string;
 }
 
 interface smallTableProps {
@@ -23,8 +23,7 @@ export default function SmallTable({
   headerTitles,
   data,
 }: smallTableProps) {
-  
-  console.log("Are data OK?: ", data)
+  console.log("Are data OK?: ", data);
 
   return (
     <div className="tw-relative tw-overflow-x-auto tw-shadow-md sm:tw-rounded-lg tw-px-6 tw-pt-4 tw-pb-6 tw-bg-white">
@@ -35,14 +34,14 @@ export default function SmallTable({
               {caption.name} ({caption.amount})
             </div>
             <div className="tw-flex tw-items-center tw-cursor-pointer">
-              <div className="tw-text-right tw-underline tw-text-mid-pale-blue tw-text-sm tw-items-center">
+              <div className="tw-text-right tw-text-mid-pale-blue tw-text-sm tw-items-center">
                 Show All
               </div>
               <KeyboardArrowRight className="tw-text-mid-pale-blue" />
             </div>
           </div>
         </caption>
-        <thead className="tw-text-xs tw-text-mid-pale-blue tw-capitalize tw-bg-light-pale-blue-white">
+        <thead className="tw-text-center tw-text-xs tw-text-mid-pale-blue tw-capitalize tw-bg-light-pale-blue-white">
           <tr>
             {headerTitles.map((el, index) => (
               <th scope="col" key={index} className="tw-px-6 tw-py-3">
@@ -53,17 +52,26 @@ export default function SmallTable({
         </thead>
         {
           <tbody>
-            {data.map((e, index) => (
-              <tr key={index} className="tw-bg-white tw-border-b tw-border-light-sky-blue">
+            {data.slice(0,3).map((e, index) => (
+              <tr
+                key={index}
+                className="tw-bg-white tw-border-b tw-border-light-sky-blue"
+              >
                 <th
                   scope="row"
-                  className="tw-px-6 tw-py-4 tw-font-medium tw-text-gray-900 tw-whitespace-nowrap "
+                  className="tw-text-center tw-px-6 tw-py-4 tw-font-medium tw-text-gray-900 tw-whitespace-nowrap tw-capitalize"
                 >
                   {e.role}
                 </th>
-                <td className="tw-px-6 tw-py-4">{e.first_name}</td>
-                <td className="tw-px-6 tw-py-4">{e.last_name}</td>
-                <td className="tw-px-6 tw-py-4">{e.active}</td>
+                <td className="tw-px-4 tw-py-2 tw-text-center">{e.first_name}</td>
+                <td className="tw-px-4 tw-py-2 tw-text-center">{e.last_name}</td>
+                <td className="tw-px-4 tw-py-2 tw-flex tw-text-center tw-items-center tw-justify-center tw-align-middle">
+                  <div
+                    className={`tw-flex tw-w-10 tw-h-10 tw-rounded-full tw-items-center tw-justify-center  ${
+                      (e.active === "active") ? "tw-bg-green-correct" : "tw-bg-red-incorrect"
+                    }`}
+                  >{(e.active === "active" ) ? "Yes" : "No"}</div>
+                </td>
               </tr>
             ))}
           </tbody>
