380fafe1e8dba763bf18a8bcbb9c5fc67dcb5984
Michael <mixkok2001@gmail.com>
2024-12-01 20:40:19 +0200

    Added white background and changed padding+margin of different divs

--- tms-frontend/src/components/content/UsersTable.jsx
+++ tms-frontend/src/components/content/UsersTable.jsx
@@ -563,7 +563,7 @@ export default function UsersTable() {
   }
 
   return (
-    <div className="tables-data">
+    <div className="tables-data tw-bg-white tw-px-4 tw-py-6 tw-rounded-2xl">
       <div className="users-container">
         <ConfirmationModal
           show={showConfirmation}
@@ -582,31 +582,26 @@ export default function UsersTable() {
           {message}
         </Alert>
 
-        <div className="filter-content">
+        <div className="tw-flex filter-content tw-justify-start">
           {" "}
           {/* Search Functionality */}
-          <div className="">
-            <label htmlFor={"table-search"} className="tw-sr-only">
-              Search
-            </label>
-            <div className="tw-relative tw-mt-1 tw-text-gray-300">
-              <div className="tw-absolute tw-inset-y-0 tw-start-0 tw-flex tw-items-center tw-ps-3 tw-pointer-events-none">
-                <SearchIcon />
-              </div>
-              <input
-                type="text"
-                id="table-search"
-                className="tw-flex tw-flex-1 tw-items-center tw-py-2 tw-ps-10 tw-text-sm tw-text-dark-sky-blue tw-border tw-border-light-blue tw-rounded-lg tw-w-80 tw-bg-light-pale-blue-white focus:tw-outline-none focus:tw-ring-mid-pale-blue focus:tw-border-mid-pale-blue"
-                placeholder="Search for people"
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-              />
+          <div className="tw-relative tw-mt-1 tw-text-gray-300 tw-mb-6">
+            <div className="tw-absolute tw-inset-y-0 tw-start-0 tw-flex tw-items-center tw-ps-3 tw-pointer-events-none">
+              <SearchIcon />
             </div>
+            <input
+              type="text"
+              id="table-search"
+              className="tw-flex tw-flex-1 tw-items-center tw-py-2 tw-ps-10 tw-text-sm tw-text-dark-sky-blue tw-border tw-border-light-blue tw-rounded-lg tw-w-80 tw-bg-light-pale-blue-white focus:tw-outline-none focus:tw-ring-mid-pale-blue focus:tw-border-mid-pale-blue"
+              placeholder="Search for people"
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+            />
           </div>
         </div>
 
         <Table className="users-table" hover size="md" responsive>
-          <thead>
+          <thead className="tw-text-xs tw-text-mid-pale-blue tw-capitalize tw-bg-light-pale-blue-white">
             <tr>
               <th>#</th>
               <th className="table-header">
