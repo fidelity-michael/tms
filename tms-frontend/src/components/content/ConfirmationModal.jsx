e3f13072842fc9e323f28378a4c7435aece053ef
Michael <mixkok2001@gmail.com>
2024-12-06 00:32:55 +0200

    Changed buttons to match color style of application in Confirmation Modal

--- tms-frontend/src/components/content/ConfirmationModal.jsx
+++ tms-frontend/src/components/content/ConfirmationModal.jsx
@@ -1,54 +1,59 @@
 import React from "react";
-import { Button, Modal } from 'react-bootstrap';
-import axios from 'axios';
+import { Button, Modal } from "react-bootstrap";
+import axios from "axios";
 
-export default function ConfirmationModal({ show, setShow, path, setResponse, message }) {
+export default function ConfirmationModal({
+  show,
+  setShow,
+  path,
+  setResponse,
+  message,
+}) {
+  function cancel(target) {
+    setResponse("canceled");
+    setShow(false);
+  }
 
-    function cancel(target) {
-        setResponse("canceled");
-        setShow(false);
-    }
+  function confirm(e) {
+    // console.log("Delete: " + path);
+    axios
+      .delete(path)
+      .then((data) => {
+        setResponse("deleted");
+      })
+      .catch((err) => {
+        setResponse("failed");
+        console.log(err);
+      });
 
-    function confirm(e) {
-        // console.log("Delete: " + path);
-        axios.delete(path)
-            .then((data) => {
-                setResponse("deleted");
-            })
-            .catch((err) => {
-                setResponse("failed");
-                console.log(err);
-            });
+    setShow(false);
+  }
 
-        setShow(false);
-    }
+  return (
+    <Modal show={show} onHide={(e) => setShow(false)} animation={false}>
+      <Modal.Header closeButton>
+        <Modal.Title>Confirm Action</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {message ? <p>{message}</p> : <p>This can't be undone</p>}
+      </Modal.Body>
+      <Modal.Footer>
 
-    return (
-        <Modal
-            show={show}
-            onHide={(e) => setShow(false)}
-            animation={false}
-        >
-            <Modal.Header closeButton>
-                <Modal.Title>Confirm Action</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                {
-                    message ?
-                        <p>{message}</p>
-                    :
-                        <p>This can't be undone</p>
-                }
-                
-            </Modal.Body>
-            <Modal.Footer>
-                <Button className="cancel-modal" variant="light" onClick={(e) => cancel(e.target)}>
-                    Cancel
-          </Button>
-                <Button className="confirm-modal btn-danger" variant="light" onClick={(e) => confirm(e.target)}>
-                    Delete
-          </Button>
-            </Modal.Footer>
-        </Modal>
-    )
+        <div className="tw-flex tw-gap-1">
+          <button
+            onClick={(e) => cancel(e.target)}
+            className="tw-bg-transparent hover:tw-bg-dark-sky-blue tw-text-dark-sky-blue tw-font-semibold hover:tw-text-white tw-py-2 tw-px-4 tw-border tw-border-dark-sky-blue hover:tw-border-transparent tw-rounded"
+          >
+            Cancel
+          </button>
+          <button
+            onClick={(e) => confirm(e.target)}
+            className="tw-font-semibold tw-text-white tw-bg-red-incorrect tw-py-2 tw-px-4 tw-border-dark-sky-blue hover:tw-opacity-95 hover:tw-text-dark-sky-blue hover:tw-border-dark-sky-blue tw-rounded"
+          >
+            Delete
+          </button>
+        </div>
+      </Modal.Footer>
+    </Modal>
+  );
 }
