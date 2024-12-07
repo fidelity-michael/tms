729b917b2b3110b7c4dc9dea36e09f142121bdbe
Michael <mixkok2001@gmail.com>
2024-12-07 17:37:29 +0200

    Added custom pie chart for statistics

--- tms-frontend/src/components/content/Statistics.tsx
+++ tms-frontend/src/components/content/Statistics.tsx
@@ -1,35 +1,35 @@
 // import Iframe from 'react-iframe'
-/* import {
+import {
   Card,
   CardBody,
   CardHeader,
   Typography,
 } from "@material-tailwind/react";
 import Chart from "react-apexcharts";
-import { Square3Stack3DIcon } from "@heroicons/react/24/outline"; */
 
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 type NumberCardProps = {
-  number: number; // Update to accept a resolved number
+  number: number;
   title: string;
   description: string;
 };
 
 const getUsers = async () => {
   const res = await axios.get("/api/users");
-  return res.data.length || 0;
+
+  return res.data;
 };
 
 const getTheses = async () => {
   const res = await axios.get("/api/theses");
-  return res.data.length || 0;
+  return res.data;
 };
 
 const getThesesRequests = async () => {
   const res = await axios.get("/api/theses_requests");
-  return res.data.length || 0;
+  return res.data;
 };
 
 function NumberCard({ number, title, description }: NumberCardProps) {
@@ -44,7 +44,170 @@ function NumberCard({ number, title, description }: NumberCardProps) {
   );
 }
 
+function PieChart({ chartConfig }: any) {
+  return (
+    <Card className="tw-rounded-xl">
+      <CardHeader
+        floated={false}
+        shadow={false}
+        color="transparent"
+        className="flex flex-col gap-4 rounded-none md:flex-row md:items-center"
+      >
+        <div className="tw-p-5">
+          <Typography
+            placeholder={""}
+            variant="h6"
+            color="blue-gray"
+            className="tw-text-dark-sky-blue tw-text-xl"
+          >
+            Registered Users
+          </Typography>
+          {/* <Typography
+            placeholder={""}
+            variant="small"
+            color="gray"
+            className="tw-max-w-sm tw-font-normal"
+          ></Typography> */}
+        </div>
+      </CardHeader>
+      <CardBody
+        placeholder={""}
+        className="tw-mt-4 tw-grid tw-place-items-center tw-px-2"
+      >
+        <Chart {...chartConfig} />
+      </CardBody>
+    </Card>
+  );
+}
+
+type User = {
+  _id: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  password: string;
+  role: string[]; // An array of roles, e.g., ["student"]
+  group: string;
+  status: string; // e.g., "active" or "inactive"
+  department: string; // Assuming it's an ID reference to a department
+  date: string; // ISO date string
+  createdAt: string; // ISO date string
+  updatedAt: string; // ISO date string
+};
+
+type chartConfigForUsersProps = {
+  users: User[];
+};
+
+/* function chartConfigForUsers({ users }: chartConfigForUsersProps) {
+  let studentCount = 0;
+  let adminCount = 0;
+  let professorCount = 0;
+  let secretariantCount = 0;
+
+  users.map((el) => {
+    el.role.map((role) => {
+      if (role === "student") studentCount++;
+      else if (role === "professor") professorCount++;
+      else if (role === "secretariat") secretariantCount++;
+      else if (role === "administrator") adminCount++;
+    });
+  });
+
+  return {
+    type: "pie",
+    width: 280,
+    height: 280,
+    series: [adminCount, secretariantCount, professorCount, studentCount],
+    options: {
+      chart: {
+        toolbar: {
+          show: false,
+        },
+      },
+      title: {
+        show: "",
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      colors: ["#020617", "#ff8f00", "#00897b", "#1e88e5"],
+      legend: {
+        show: false,
+      },
+    },
+  };
+} */
+
+function chartConfigForUsers({ users }: chartConfigForUsersProps) {
+  if (!users || users.length === 0) {
+    return {
+      type: "pie",
+      width: 280,
+      height: 280,
+      series: [0, 0, 0, 0],
+      options: {
+        chart: {
+          toolbar: {
+            show: false,
+          },
+        },
+        title: {
+          show: "",
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        colors: ["#020617", "#ff8f00", "#00897b", "#1e88e5"],
+        legend: {
+          show: false,
+        },
+      },
+    };
+  }
+
+  let studentCount = 0;
+  let adminCount = 0;
+  let professorCount = 0;
+  let secretariantCount = 0;
+
+  users.map((el) => {
+    el.role.map((role) => {
+      if (role === "student") studentCount++;
+      else if (role === "professor") professorCount++;
+      else if (role === "secretariat") secretariantCount++;
+      else if (role === "administrator") adminCount++;
+    });
+  });
+
+  return {
+    type: "pie",
+    width: 450,
+    height: 450,
+    series: [adminCount, secretariantCount, professorCount, studentCount],
+    options: {
+      labels: ["Admins", "Secretariants", "Professors", "Students"],
+      chart: {
+        toolbar: {
+          show: false,
+        },
+      },
+      title: {
+        show: "",
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      colors: ["#384959", "#ff8f00", "#00897b", "#1e88e5"],
+      legend: {
+        show: true,
+      },
+    },
+  };
+}
+
 export default function Statistics() {
+  const [users, setUsers] = useState<User[]>([]);
   const [totalUsers, setTotalUsers] = useState<number>(0);
   const [totalTheses, setTotalTheses] = useState<number>(0);
   const [totalThesesRequests, setTotalThesesRequests] = useState<number>(0);
@@ -55,10 +218,12 @@ export default function Statistics() {
       const theses = await getTheses();
       const thesesRequests = await getThesesRequests();
 
-      setTotalUsers(users);
-      setTotalTheses(theses);
-      setTotalThesesRequests(thesesRequests);
+      setUsers(users);
+      setTotalUsers(users.length || 0);
+      setTotalTheses(theses.length || 0);
+      setTotalThesesRequests(thesesRequests.length || 0);
     };
+    console.log("SET?? USERS???: ", users);
 
     fetchData();
   }, []);
@@ -90,8 +255,10 @@ export default function Statistics() {
           </div>
         </div>
         <div className="row">
-          <div className="col">hey</div>
-          <div className="col">yo</div>
+          <div className="col">
+            <PieChart chartConfig={chartConfigForUsers({ users })} />
+          </div>
+          <div className="col"></div>
         </div>
       </div>
     );
@@ -99,7 +266,7 @@ export default function Statistics() {
 
   return (
     <div className="stats-wrapper">
-      <h5>Charts</h5>
+      <h5 className="tw-text-3xl tw-text-dark-sky-blue tw-mb-2">Charts</h5>
       <div id="chart"></div>
       {renderCharts()}
     </div>
