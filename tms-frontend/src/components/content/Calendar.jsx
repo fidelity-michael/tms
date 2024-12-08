6aa3cedae003169f0e4b494ef3be84e8a3add796
Michael <mixkok2001@gmail.com>
2024-12-08 15:43:06 +0200

    Removed button that adds events and instead used cutom classes and a new plugin to enable the user to click the date he wants.

--- tms-frontend/src/components/content/Calendar.jsx
+++ tms-frontend/src/components/content/Calendar.jsx
@@ -1,66 +1,75 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios';
-import CalendarEventModal from '../../components/content/CalendarEventModal'
-import FullCalendar from '@fullcalendar/react' // must go before plugins
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
-import Tippy from 'tippy.js';
-import 'tippy.js/dist/tippy.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import CalendarEventModal from "../../components/content/CalendarEventModal";
+import FullCalendar from "@fullcalendar/react"; // must go before plugins
+import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+import Tippy from "tippy.js";
+import "tippy.js/dist/tippy.css";
+import AddIcon from "@mui/icons-material/Add";
 
-function Calendar({userId}) {
+import interactionPlugin from "@fullcalendar/interaction";
 
-    const [showCalendar, setShowCalendar] = useState(false);
-    const [events, setEvents] = useState([]);
-    
-    useEffect(() => {
-        const getEvents = async ()  => {
-            await axios.get('/api/calendarEvents/'+userId)
-            .then((res) => {
-                console.log(res.data)
-                setEvents(res.data)
-            })
-            .catch(() => {
-                console.log("Server Internal error occured!")
-            })
-        }
+function Calendar({ userId }) {
+  const [showCalendar, setShowCalendar] = useState(false);
+  const [events, setEvents] = useState([]);
 
-        if(userId){
-            getEvents()
-        }
-
-    }, [userId])
-
-    function fetchEvents(events){
-        setEvents(events)
-    }
-
-    const handleMouseEnter = (arg) =>{
-        Tippy(arg.el, {
-            content: arg.event._def.title + " at " + arg.timeText
+  useEffect(() => {
+    const getEvents = async () => {
+      await axios
+        .get("/api/calendarEvents/" + userId)
+        .then((res) => {
+          setEvents(res.data);
+        })
+        .catch(() => {
+          console.log("Server Internal error occured!");
         });
-        console.log(arg)
-    }
+    };
 
-    return (
-        <div>
-            <button className="btn btn-info calendarBtn" onClick={() => {setShowCalendar(true)}}>New Event</button>
-            
-            <div className="calendarContainer" >
-                <FullCalendar
-                    events={events}
-                    plugins={[ dayGridPlugin ]}
-                    initialView="dayGridMonth"
-                    eventDidMount={handleMouseEnter}
-                />
-            </div> 
-            
-            <CalendarEventModal 
-                userId={userId} 
-                show={showCalendar} 
-                fetchEvents={fetchEvents}
-                onShow={(data) => setShowCalendar(data)} 
-            />
+    if (userId) {
+      getEvents();
+    }
+  }, [userId]);
+
+  function fetchEvents(events) {
+    setEvents(events);
+  }
+
+  const handleMouseEnter = (arg) => {
+    Tippy(arg.el, {
+      content: arg.event._def.title + " at " + arg.timeText,
+    });
+  };
+
+  return (
+    <div className="tw-flex tw-flex-col tw-gap-4">
+      <div className="tw-flex tw-justify-between tw-mb-4">
+        <div className="tw-text-dark-sky-blue tw-text-md">
+          <a href="">Dashboard</a> &gt; Calendar
         </div>
-    )
+      </div>
+
+      {/* NOTE: Overriding cells functionality @App.css with provided classes */}
+      <div className="tw-text-dark-sky-blue">
+        <FullCalendar
+          events={events}
+          plugins={[dayGridPlugin, interactionPlugin]}
+          initialView="dayGridMonth"
+          eventDidMount={handleMouseEnter}
+          eventColor="#88BDF2"
+          dateClick={() => {
+            setShowCalendar(true);
+          }}
+        />
+      </div>
+
+      <CalendarEventModal
+        userId={userId}
+        show={showCalendar}
+        fetchEvents={fetchEvents}
+        onShow={(data) => setShowCalendar(data)}
+      />
+    </div>
+  );
 }
 
-export default Calendar
+export default Calendar;
