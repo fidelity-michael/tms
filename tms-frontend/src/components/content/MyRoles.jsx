0b1e00993e26202ff866537207e5e5e1354bc358
Michael <mixkok2001@gmail.com>
2024-12-08 16:37:07 +0200

    Added tab history in My roles page

--- tms-frontend/src/components/content/MyRoles.jsx
+++ tms-frontend/src/components/content/MyRoles.jsx
@@ -1,68 +1,90 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom"; // Changed from deprecated useHistory
-import axios from 'axios';
-import './content.css';
+import axios from "axios";
+import "./content.css";
 
-export default function MyRoles({userId, currentRole}) {;
-    
-    const [roles, setRoles] = useState([]);
-    const navigate = useNavigate();
+export default function MyRoles({ userId, currentRole }) {
+  const [roles, setRoles] = useState([]);
+  const navigate = useNavigate();
 
-    //fetch user's data
-    useEffect(() => {
-        axios.get('api/users/'+userId)
-            .then((res) => {
-                setRoles(res.data.role)
-            })
-            .catch(() => {
-                console.log('Server internal error occurred!');
-            });
-    
-    }, [userId])
+  //fetch user's data
+  useEffect(() => {
+    axios
+      .get("api/users/" + userId)
+      .then((res) => {
+        setRoles(res.data.role);
+      })
+      .catch(() => {
+        console.log("Server internal error occurred!");
+      });
+  }, [userId]);
 
-    function showInfo(role){
-        document.getElementById("info"+role).innerHTML = "Go to my "+role+" page"
-    }
+  function showInfo(role) {
+    document.getElementById("info" + role).innerHTML =
+      "Go to my " + role + " page";
+  }
 
-    function hideInfo(role){
-        document.getElementById("info"+role).innerHTML = ""
-    }
+  function hideInfo(role) {
+    document.getElementById("info" + role).innerHTML = "";
+  }
 
-    function changeRolePage(role){
-        navigate('/'+role, { state: "changeRole" });
-    }
+  function changeRolePage(role) {
+    navigate("/" + role, { state: "changeRole" });
+  }
 
-    return (
-        <div>
-            <ul>
-                {
-                    roles.map((role) => {
-                        if(role === currentRole){
-                            return(
-                                <li className="liRole" key={userId+role} style={{fontSize: "1.5rem"}}>
-                                    <b>{role}</b>
-                                </li>
-                            )
-                        } else {
-                            return(
-                                <li className="liRole" key={userId+role} style={{fontSize: "1.5rem"}}>
-                                    <div>
-                                        <button type="button" className="btn btn-info roleButton" 
-                                            onMouseOver={() => {showInfo(role)}} 
-                                            onMouseOut={() => {hideInfo(role)}}
-                                            onClick={() => {changeRolePage(role)}}
-                                        >
-                                            {role}
-                                        </button>
-                                        <small id={"info"+role} style={{display: "block"}}></small>
-                                    </div>
-                                </li>
-                            )
-                        }
-                    })
-                }
-            </ul>
-            
+  return (
+    <div>
+      <div className="tw-flex tw-justify-between tw-mb-8">
+        <div className="tw-text-dark-sky-blue tw-text-md">
+          <a href="">Dashboard</a> &gt; Calendar
         </div>
-    )
+      </div>
+
+      <ul>
+        {roles.map((role) => {
+          if (role === currentRole) {
+            return (
+              <li
+                className="liRole tw-text-dark-sky-blue"
+                key={userId + role}
+                style={{ fontSize: "1.5rem" }}
+              >
+                <b>{role}</b>
+              </li>
+            );
+          } else {
+            return (
+              <li
+                className="liRole tw-text-dark-sky-blue"
+                key={userId + role}
+                style={{ fontSize: "1.5rem" }}
+              >
+                <div>
+                  <button
+                    type="button"
+                    className="btn btn-info roleButton"
+                    onMouseOver={() => {
+                      showInfo(role);
+                    }}
+                    onMouseOut={() => {
+                      hideInfo(role);
+                    }}
+                    onClick={() => {
+                      changeRolePage(role);
+                    }}
+                  >
+                    {role}
+                  </button>
+                  <small
+                    id={"info" + role}
+                    style={{ display: "block" }}
+                  ></small>
+                </div>
+              </li>
+            );
+          }
+        })}
+      </ul>
+    </div>
+  );
 }
