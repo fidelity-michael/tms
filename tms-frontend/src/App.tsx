cb4cd46d08a4e4c926bb922a179eb1e68bb65ea7
Michael <mixkok2001@gmail.com>
2024-12-10 01:46:24 +0200

    App.tsx formatted

--- tms-frontend/src/App.tsx
+++ tms-frontend/src/App.tsx
@@ -1,9 +1,6 @@
-import { BrowserRouter as Router, 
-  Route, 
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import LoginPage from "./webpages/LoginPage/LoginPage"
+import LoginPage from "./webpages/LoginPage/LoginPage";
 import StudentPage from "./webpages/StudentPage/StudentPage";
 import ProfessorPage from "./webpages/ProfessorPage/ProfessorPage";
 import SecretariatPage from "./webpages/SecretariatPage/SecretariatPage";
@@ -14,18 +11,17 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "./App.css";
 
-
 function App() {
   return (
     <Router>
       <div className="app-container">
         <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/student" element={<StudentPage />} />
-            <Route path="/professor" element={<ProfessorPage />} />
-            <Route path="/secretariat" element={<SecretariatPage />} />
-            <Route path="/administrator" element={<AdministatorPage />} />
-            <Route path="*" element={<NotFoundPage />} />
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/student" element={<StudentPage />} />
+          <Route path="/professor" element={<ProfessorPage />} />
+          <Route path="/secretariat" element={<SecretariatPage />} />
+          <Route path="/administrator" element={<AdministatorPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </div>
     </Router>
