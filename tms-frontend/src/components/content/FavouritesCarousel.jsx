bdd6efd6263e382621e3c1de05f38e2b366cac52
Michael <mixkok2001@gmail.com>
2024-12-11 02:34:40 +0200

    Customized Carousel for favourite areas

--- tms-frontend/src/components/content/FavouritesCarousel.jsx
+++ tms-frontend/src/components/content/FavouritesCarousel.jsx
@@ -1,121 +1,122 @@
-import { useState, useEffect, useRef } from 'react';
-import { Card } from 'react-bootstrap';
-import Carousel from '@itseasy21/react-elastic-carousel'; // NOTE: Changed import because of dependency conflicts
+import { useState, useEffect, useRef } from "react";
+import { Card } from "react-bootstrap";
+import Carousel from "@itseasy21/react-elastic-carousel"; // NOTE: Changed import because of dependency conflicts
 // import axios from 'axios';
-import './content.css';
+import "./content.css";
 
 export default function FavouritesCarousel({ userId, studentFavourites }) {
-    const [items, setItems] = useState(3);
-    const [favourites, setFavourites] = useState([]);
-    // const [loadingFavourites, setLoadingFavourites] = useState(false);
-    const loadingFavourites = false;
+  const [items, setItems] = useState(3);
+  const [favourites, setFavourites] = useState([]);
+  // const [loadingFavourites, setLoadingFavourites] = useState(false);
+  const loadingFavourites = false;
 
-    const componentIsMounted = useRef(true)
+  const componentIsMounted = useRef(true);
 
-    function getRandomImage(){
-        return "src/components/images/computer_science_" + Math.floor(Math.random() * 7).toString() + ".jpg";
-    }
-    useEffect(() => {
-        const handleResize = () => {
-            if (window.innerWidth < 600) {
-                if (componentIsMounted.current) setItems(1);
-            }
-            else if (window.innerWidth < 900) {
-                if (componentIsMounted.current) setItems(2);
-            }
-            else {
-                if (componentIsMounted.current) setItems(3);
-            }
-        }
+  function getRandomImage() {
+    return (
+      "src/components/images/computer_science_" +
+      Math.floor(Math.random() * 7).toString() +
+      ".jpg"
+    );
+  }
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 600) {
+        if (componentIsMounted.current) setItems(1);
+      } else if (window.innerWidth < 900) {
+        if (componentIsMounted.current) setItems(2);
+      } else {
+        if (componentIsMounted.current) setItems(3);
+      }
+    };
 
-        // const fetchFavourites = async () => {
-        //     try {
-        //         if (componentIsMounted.current) setLoadingFavourites(true);
-        //         const favourites_data = await axios.get('/api/favourites/' + userId);
-        //         // console.log(favourites_data.data);
-        //         if (favourites_data.data.length > 0) {
-        //             if (componentIsMounted.current) {
-        //                 setFavourites(favourites_data.data);
-        //                 setLoadingFavourites(false);
-        //             }
-        //         }
-        //     }
-        //     catch (err) {
-        //         console.log("Server internal error occurred!", err);
-        //     }
-        // }
-
-        window.addEventListener("resize", handleResize);
-        handleResize();
-        // fetchFavourites();
-
-        return () => {
-            componentIsMounted.current = true
-            // componentIsMounted.current = false
-        }
-    }, [userId]);
-
-    useEffect(() => {
-        setFavourites(studentFavourites);
-    }, [studentFavourites]);
-
-    // function toggleFavourite(e) {
-    //     console.log('Added to favourites!');
+    // const fetchFavourites = async () => {
+    //     try {
+    //         if (componentIsMounted.current) setLoadingFavourites(true);
+    //         const favourites_data = await axios.get('/api/favourites/' + userId);
+    //         // console.log(favourites_data.data);
+    //         if (favourites_data.data.length > 0) {
+    //             if (componentIsMounted.current) {
+    //                 setFavourites(favourites_data.data);
+    //                 setLoadingFavourites(false);
+    //             }
+    //         }
+    //     }
+    //     catch (err) {
+    //         console.log("Server internal error occurred!", err);
+    //     }
     // }
 
-    function renderCarousel() {
-        return (
-            <Carousel className='fav-carousel'
-                pagination={false}
-                enableSwipe={false}
-                itemPadding={[10, 10]}
-                // enableAutoPlay
-                // autoPlaySpeed={7500}
-                itemsToShow={items}
-                isRTL={false}
-            >
-                {
-                    renderFavourites()
-                }
-            </Carousel>
-        );
-    }
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    // fetchFavourites();
 
-    function renderFavourites() {
-        return favourites.map((favourite) => {
-            const { area_id, area_name } = favourite;
-            return (
-                <div key={area_id} className='card-container'>
-                    <Card className='card'>
-                        {/* <span className="badge heart"><i id={_id} className="fas fa-heart" onClick={(e) => toggleFavourite(e.target)}></i></span> */}
-                        <Card.Img variant="top" src={getRandomImage()} />
-                        <Card.Body>
-                            <Card.Title>{area_name}</Card.Title>
-                            {/* <Card.Text>
-                                {area_name}
-                            </Card.Text> */}
-                        </Card.Body>
-                    </Card>
-                </div>
-            )
-        });
-    }
+    return () => {
+      componentIsMounted.current = true;
+      // componentIsMounted.current = false
+    };
+  }, [userId]);
 
-    function loadingData() {
-        return (
-            <div className="d-flex justify-content-center text-primary fav-loading">
-                <div className="spinner-border spinner-border-lg" role="status">
-                    <span className="sr-only">Loading...</span>
-                </div>
-            </div>
-        );
-    }
+  useEffect(() => {
+    setFavourites(studentFavourites);
+  }, [studentFavourites]);
 
+  // function toggleFavourite(e) {
+  //     console.log('Added to favourites!');
+  // }
+
+  function renderCarousel() {
     return (
-        <div className='fav-favourites'>
-            {
-                loadingFavourites ? loadingData() : (favourites.length ? renderCarousel() : null)
-            }
-        </div>
+      <Carousel
+        className="fav-carousel "
+        pagination={false}
+        enableSwipe={false}
+        itemPadding={[10, 10]}
+        // enableAutoPlay
+        // autoPlaySpeed={7500}
+        itemsToShow={items}
+        isRTL={false}
+      >
+        {renderFavourites()}
+      </Carousel>
     );
+  }
+
+  function renderFavourites() {
+    return favourites.map((favourite) => {
+      const { area_id, area_name } = favourite;
+      return (
+        <div key={area_id} className="card-container">
+          <Card className="card tw-rounded-lg">
+            <Card.Img variant="top" src={getRandomImage()} />
+            <Card.Body>
+              <Card.Title className="tw-text-dark-sky-blue">
+                {area_name}
+              </Card.Title>
+            </Card.Body>
+          </Card>
+        </div>
+      );
+    });
+  }
+
+  function loadingData() {
+    return (
+      <div className="d-flex justify-content-center text-primary fav-loading">
+        <div className="spinner-border spinner-border-lg" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="fav-favourites">
+      {loadingFavourites
+        ? loadingData()
+        : favourites.length
+          ? renderCarousel()
+          : null}
+    </div>
+  );
 }
